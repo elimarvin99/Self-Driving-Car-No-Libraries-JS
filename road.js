f3f03ad4ad5f974703ce68d9eaf7ba420164bd20
Eli Marvin <emarvin@paradigmseniorservices.com>
2022-05-15 19:17:28 -0400

    Made road with lanes. next step is to add bordres to the road so the car can't go off the screen

---
+++ road.js
@@ -0,0 +1,52 @@
+class Road{
+    //centered around an x value and have a width
+    constructor(x, width, laneCount = 3){
+        this.x = x;
+        this.width = width;
+        this.laneCount = laneCount;
+
+        this.left = x - width / 2;
+        this.right = x + width / 2;
+
+        const infinity = 1000000;
+        this.top =- infinity;
+        this.bottom = infinity;
+
+        
+    }
+
+    //this method gets the lane center to place the car properly
+    getLaneCenter(laneIndex){
+        //gets the width of each lane
+        const laneWidth = this.width / this.laneCount;
+        //divided by 2 so the car is in the center of the lane
+        return this.left + laneWidth / 2 + 
+        //accounts for car to always be on the last lane and never off the screen
+        Math.min(laneIndex, this.laneCount - 1) * laneWidth; 
+    }
+
+    //this method draws the road
+    draw(ctx){
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "white";
+
+        for(let i = 0; i <= this.laneCount; i++){
+            //the x coordinate of each lane/line depends on the amount of lanes
+            const x = lerp(
+                this.left,
+                this.right,
+                i / this.laneCount
+            );
+            //this creates dashes in the inner lanes
+            if(i > 0 && i < this.laneCount){
+                ctx.setLineDash([20,20]);
+            }else{
+                ctx.setLineDash([]);
+            }
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
+        }
+    }
+}
---
+++ utils.js
@@ -0,0 +1,4 @@
+function lerp(A, B, t){
+    //the difference between two points returned based on a percentage
+    return A + (B - A) * t;
+}
